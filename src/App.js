f09d49d0e0b48f3282d05ecd4f9f9de15fe7e373
Nick De Marco <nickdmrco@gmail.com>
2022-06-07 14:37:33 -0700

    server changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <NavBar />
 
       <Routes>
-        <Route path='/portfolio22' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
